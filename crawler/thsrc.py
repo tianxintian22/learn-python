94ca35d6744fcd31a8efd33215e738cc28966545
unknown <1247016369@qq.com>
2018-03-08 14:02:07 +0800

    urllib

---
+++ crawler/thsrc.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+
+import urllib2
+import urllib
+
+url = 'http://www.thsrc.com.tw/tw/TimeTable/SearchResult'
+request = urllib2.Request(url)
+
+request.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36')
+
+post_data = {
+    'StartStation' : '9c5ac6ca-ec89-48f8-aab0-41b738cb1814',
+    'EndStation' : '977abb69-413a-4ccf-a109-0272c24fd490',
+    'SearchDate' : '2018/03/08',
+    'SearchTime ': '10:00',
+    'SearchWay' : 'DepartureInMandarin'
+}
+data = urllib.urlencode(post_data)
+request.add_data(data)
+
+response = urllib2.urlopen(request)
+
+print response.read()
